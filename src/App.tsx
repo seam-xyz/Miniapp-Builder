cdf2b531a37f2aeaeb4b16021a78502d2001e44f
Nick Confrey <nickconfrey@earthlink.net>
2022-11-29 16:20:35 -0500

    implement the mock grid layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
+import "./styles.css";
+import { makeStyles } from "@material-ui/core/styles";
+import { useState } from "react";
+import  { Responsive, WidthProvider } from "react-grid-layout";
+
+const ResponsiveReactGridLayout = WidthProvider(Responsive);
+
+const useStyles = makeStyles(() => ({
+  root: {
+    display: "flex",
+    height: "100%",
+    width: "100%",
+    flexDirection: "column"
+  },
+}));
 
 function App() {
+  const classes = useStyles();
+  let defaultLayout: ReactGridLayout.Layouts = { lg: [{ w: 4, h: 15, x: 0, y: 0, i: "0" }] }
+  const [layouts, setLayouts] = useState(defaultLayout);
+
+  const onLayoutChange = (currentLayout: ReactGridLayout.Layout[], allLayouts: ReactGridLayout.Layouts) => {
+    setLayouts(allLayouts);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.root}>
+      <h1>Seam Block SDK</h1>
+      <ResponsiveReactGridLayout
+        className="layout"
+        layouts={layouts}
+        breakpoints={{ lg: 1200, xxs: 0 }}
+        cols={{ lg: 12, xxs: 2 }}
+        rowHeight={5}
+        onLayoutChange={onLayoutChange}
+        resizeHandles={['se']}
+        measureBeforeMount={true}
+      >
+          {/* <Widget id={model.uuid}
+                    onRemoveItem={onRemoveItem}
+                    onEditItem={onEditItem}
+                    backgroundColor="transparent"
+                    isInEditMode={isInEditMode}>
+                    {renderBlock(model)}
+                  </Widget> */}
+          <h1>Block 1</h1>
+      </ResponsiveReactGridLayout>
     </div>
   );
 }
