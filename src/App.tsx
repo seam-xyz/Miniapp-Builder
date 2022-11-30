47b78e6bbfb828989031b2481e80eb1ebf9e2d4d
Nick Confrey <nickconfrey@earthlink.net>
2022-11-30 15:55:20 -0500

    rearchetect the testing harness to split out app from content, to reduce the amount of overhead and system internals when specifying your block type

--- src/App.tsx
+++ src/App.tsx
@@ -1,94 +1,42 @@
-
-import './App.css';
-import "./styles.css";
-import _ from "lodash";
 import { makeStyles } from "@material-ui/core/styles";
-import { useState } from "react";
-import { Responsive, WidthProvider } from "react-grid-layout";
-import Widget from "./Widget"
-import BlockFactory from './blocks/BlockFactory';
-import { BlockModel } from './blocks/types';
 
-const ResponsiveReactGridLayout = WidthProvider(Responsive);
+import Content from "./Content";
+import { BlockModel } from "./blocks/types";
 
-const useStyles = makeStyles(() => ({
+const drawerWidth = 240;
+
+const useStyles = makeStyles((theme) => ({
   root: {
     display: "flex",
     height: "100%",
-    width: "100%",
-    flexDirection: "column"
+    width: "100%"
+  },
+  content: {
+    flexGrow: 1,
+    height: "100%"
   },
 }));
 
-function App() {
+export default function App() {
   const classes = useStyles();
-  let defaultLayout: ReactGridLayout.Layouts = { lg: generateLayout() }
-  const [layouts, setLayouts] = useState(defaultLayout);
 
-  const onLayoutChange = (currentLayout: ReactGridLayout.Layout[], allLayouts: ReactGridLayout.Layouts) => {
-    setLayouts(allLayouts);
-  };
+  // Add your custom block here!
+  // vvvvvvvvvvvvvvvvv
 
-  /* Add your block here! */
-  // _______________________
-
-  let model: BlockModel = {
-    uuid: "test",
-    type: 'iframe',
-    data: {}
+  let yourBlock: BlockModel = {
+    type: "iframe", // rename to whatever you want your block name to be
+    data: {},
+    uuid: "test"
   }
 
-  // _______________________
-  /* End customization */
-
-  const renderBlock = (model: any) => {
-    let block = BlockFactory.getBlock(model)
-    //block.onEditCallback = onEditItem
-    return block.render()
-  }
-
-  const onRemoveItem = (itemId: any) => {
-
-  };
-  const onEditItem = (itemId: any) => {
-
-  };
+  // End customization
+  // ^^^^^^^^^^^^^^^^^
 
   return (
     <div className={classes.root}>
-      <h1>Seam Block SDK</h1>
-      <ResponsiveReactGridLayout
-        className="layout"
-        layouts={layouts}
-        breakpoints={{ lg: 1200, xxs: 0 }}
-        cols={{ lg: 12, xxs: 2 }}
-        rowHeight={5}
-        onLayoutChange={onLayoutChange}
-        resizeHandles={['se']}
-        measureBeforeMount={true}
-      >
-        <Widget id={model.uuid}
-          onRemoveItem={onRemoveItem}
-          onEditItem={onEditItem}>
-          {renderBlock(model)}
-        </Widget>
-      </ResponsiveReactGridLayout>
+      <main className={classes.content}>
+        <Content loadedBlocks={[yourBlock]} />
+      </main>
     </div>
   );
 }
-
-function generateLayout() {
-  return _.map(_.range(0, 25), function (_, i: number) {
-    var y = Math.ceil(Math.random() * 4) + 1;
-    return {
-      x: Math.round(Math.random() * 5) * 2,
-      y: Math.floor(i / 6) * y,
-      w: 2,
-      h: y,
-      i: i.toString(),
-      static: Math.random() < 0.05
-    };
-  });
-}
-
-export default App;
---
+++ src/Content.js
@@ -0,0 +1,114 @@
+// NOTE: This is a testing harness so you can resize your new block similar to how it will look on www.seam.so
+// Don't edit anything in this file, it won't be reflected in production.
+
+import React, { useState } from "react";
+import { Card, Modal } from "antd";
+//import 'antd/dist/antd.css';
+import { v1 as uuidv1 } from 'uuid';
+import { Responsive as ResponsiveGridLayout } from "react-grid-layout";
+import { withSize } from "react-sizeme";
+import Widget from "./Widget";
+
+// Blocks
+import BlockFactory from './blocks/BlockFactory';
+
+function Content({ size: { width }, loadedBlocks }) {
+  const [layouts, setLayouts] = useState({ lg: [{ x: 0, y: 0, w: 5, h: 10, i: "test" }] });
+  const [blocks, setBlocks] = useState(loadedBlocks);
+
+  const [isEditingBlock, setIsEditingBlock] = useState(-1) // number is the index of the block being edited
+  const onLayoutChange = (_, allLayouts) => {
+    setLayouts(allLayouts);
+  };
+
+  // incoming blockUUID
+  const onRemoveItem = (itemId) => {
+    setBlocks(blocks.filter((model) => model.uuid !== itemId));
+  };
+  const onEditItem = (itemId) => {
+    const indexOfEditedBlock = blocks.findIndex(element => element.uuid == itemId)
+    console.log("editing block " + indexOfEditedBlock)
+    setIsEditingBlock(indexOfEditedBlock)
+  };
+  const onAddItem = (blockName) => {
+    const newBlock = {
+      type: blockName,
+      data: {},
+      uuid: uuidv1()
+    }
+    setBlocks([...blocks, newBlock]);
+  };
+  const renderBlock = (model) => {
+    let block = BlockFactory.getBlock(model)
+    return block.render()
+  }
+  const renderBlockEditModal = () => {
+    const block = isEditingBlock != -1 ? BlockFactory.getBlock(blocks[isEditingBlock]) : null
+    return (
+      <Modal
+        visible={isEditingBlock != -1}
+        footer={null}
+        onCancel={() => setIsEditingBlock(-1)}
+        bodyStyle={{
+          padding: "15px",
+          fontSize: "17px",
+          fontWeight: "500",
+        }}
+        style={{ fontSize: "16px", fontWeight: "500" }}
+      >
+        {"Edit Block " + isEditingBlock}
+        <Card
+          style={{
+            marginTop: "10px",
+            borderRadius: "1rem",
+          }}
+          bodyStyle={{ padding: "15px" }}
+        >
+          {block?.renderEditModal(saveBlockData)}
+        </Card>
+      </Modal>
+    );
+  }
+
+  const saveBlockData = (data) => {
+    console.log("updating block data")
+    let blocksCopy = blocks
+    blocksCopy[isEditingBlock] = data
+    console.log(blocksCopy)
+    setBlocks(blocksCopy)
+    setIsEditingBlock(-1)
+  }
+
+  return (
+    <>
+      <ResponsiveGridLayout
+        className="layout"
+        layouts={layouts}
+        breakpoints={{ lg: 1200, xxs: 0 }}
+        cols={{ lg: 12, xxs: 2 }}
+        rowHeight={5}
+        width={width}
+        onLayoutChange={onLayoutChange}
+        resizeHandles={['se']}
+      >
+        {blocks.map((model, index) => (
+          <div
+            key={model.uuid}
+            className="widget"
+            data-grid={{ w: 3, h: 2, x: 0, y: Infinity }}
+          >
+            <Widget id={model.uuid}
+              onRemoveItem={onRemoveItem}
+              onEditItem={onEditItem}
+            >
+              {renderBlock(model)}
+            </Widget>
+          </div>
+        ))}
+      </ResponsiveGridLayout>
+      {renderBlockEditModal()}
+    </>
+  );
+}
+
+export default withSize({ refreshMode: "debounce", refreshRate: 60 })(Content);
