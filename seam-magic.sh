da29344de17ff74d6f48aae2d38e68f37973b43a
Nick Confrey <nickconfrey@earthlink.net>
2022-12-05 17:09:12 -0500

    update magic script to support Linux OS

--- seam-magic.sh
+++ seam-magic.sh
@@ -50,7 +50,15 @@ echo "Awesome! Sewing your $name block together..."
 # Add the new block to the types dictionary
 output="src/blocks/types.tsx"
 
-sed -i '' -e '$ d' $output
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+        sed -i -e '$ d' $output
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+        sed -i '' -e '$ d' $output
+else
+        echo "❌ Unable to create ${name}Block -- unsupported operating system."
+        exit 1
+fi
+
 echo "  \"$name\": { 
         type: \"$name\",
         displayName: \"$name\",
@@ -66,20 +74,37 @@ echo "✅ Added $name to types.tsx"
 # Create a template file for the new block
 newBlock="src/blocks/${name}Block.tsx"
 cp "src/blocks/BlockTemplate.txt" $newBlock
-sed -i '' "s/%NAME%/${name}/g" $newBlock
-echo "✅ Created ${name}Block.tsx for your new block"
+
+
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+        sed -i "s/%NAME%/${name}/g" $newBlockt
+        echo "✅ Created ${name}Block.tsx for your new block"
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+        sed -i '' "s/%NAME%/${name}/g" $newBlock
+        echo "✅ Created ${name}Block.tsx for your new block"
+fi
 
 # Add the new block to the block factory
 placeholder="\/\/ new blocks go here"
 placeholderImport="import GiphyBlock from './GiphyBlock"
 importBlock="import ${name}Block from .\/${name}Block"
 newBlockCase="case \"$name\": return new ${name}Block(model)\n      $placeholder"
-sed -i '' "s/${placeholder}/${newBlockCase}/g" "src/blocks/BlockFactory.tsx"
+
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+        sed -i "s/${placeholder}/${newBlockCase}/g" "src/blocks/BlockFactory.tsx"
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+        sed -i '' "s/${placeholder}/${newBlockCase}/g" "src/blocks/BlockFactory.tsx"
+fi
+
 #sed -i'.tsx' "1i\ $importBlock" "src/blocks/BlockFactory.tsx"
 echo "✅ Added ${name} to the BlockFactory.tsx"
 
 # Use your custom block as default in App.tsx
-sed -i '' "s/%NAME%/${name}/g" "src/App.tsx"
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+        sed -i '' "s/%NAME%/${name}/g" "src/App.tsx"
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+        sed -i "s/%NAME%/${name}/g" "src/App.tsx"
+fi
 echo "✅ Using your ${name} block as default in App.tsx"
 
 echo "
