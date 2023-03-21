0e49bdc4636a49aa41b4532c51e128c1a769d355
octopus-coder <62782124+octopus-coder@users.noreply.github.com>
2023-03-21 11:59:48 -0700

    Added Pokemon Block (#19)
    Co-authored-by: Luis Enrique <luisen.goscas@gmail.com>

--- src/blocks/BlockFactory.tsx
+++ src/blocks/BlockFactory.tsx
@@ -1,3 +1,4 @@
+import PokemonBlock from './PokemonBlock'
 import NFTsBlock from './NFTsBlock'
 import RefreshingGIFBlock from './RefreshingGIFBlock'
 import PixelArtBlock from './PixelArtBlock'
@@ -32,6 +33,7 @@ export default class BlockFactory {
       case "RefreshingGIF": return new RefreshingGIFBlock(model, theme)
       case "PixelArt": return new PixelArtBlock(model, theme)
       case "NFTs": return new NFTsBlock(model, theme)
+      case "Pokemon": return new PokemonBlock(model, theme)
       // new blocks go here
       default: return new IFrameBlock(model, theme)
     }
