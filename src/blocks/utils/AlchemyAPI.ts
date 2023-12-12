ba9a0ef6a425f2bfa1dd5a52e134c057af78e906
proofofjake <jake.galvin@btinternet.com>
2023-12-12 15:50:41 +0000

    removing my api key

--- src/blocks/utils/AlchemyAPI.ts
+++ src/blocks/utils/AlchemyAPI.ts
@@ -5,7 +5,7 @@
 import { Alchemy, Network, NftFilters } from "alchemy-sdk";
 import { all } from "axios";
 
-const ALCHEMY_API_KEY = "t4ylNl-qbBpke7O4A0fZ0Z8DFiPBbCm0" // process.env.REACT_APP_ALCHEMY_API_KEY;
+const ALCHEMY_API_KEY = process.env.REACT_APP_ALCHEMY_API_KEY;
 const ALCHEMY_NETWORK = Network.ETH_MAINNET;
 
 const alchemy = new Alchemy({ apiKey: ALCHEMY_API_KEY, network: ALCHEMY_NETWORK });
