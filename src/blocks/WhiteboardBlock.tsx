b78026b66b039929be42c6a7147346e344d2e7a1
eurbs <emileeurbanek@gmail.com>
2024-02-24 15:08:07 -0500

    refactor canvas to render simple backgrounds

--- src/blocks/WhiteboardBlock.tsx
+++ src/blocks/WhiteboardBlock.tsx
@@ -3,10 +3,123 @@ import { BlockModel } from './types'
 import BlockFactory from './BlockFactory';
 import './BlockStyles.css'
 
+import CSS from 'csstype';
+import React, { useEffect, useRef, useState } from 'react';
+
+interface CanvasInitialUserState {
+  initialBackgroundColor: string,  // Hex string e.g. '#123abc'
+}
+
+interface CanvasProps {
+  height: number,
+  width: number,
+  isEditMode: boolean
+  userInitialState: CanvasInitialUserState 
+}
+
+const Canvas: React.FC<CanvasProps> = (props: CanvasProps) => {
+  const {
+    height,
+    width,
+    isEditMode,
+  } = props;
+
+  // === Initialize ===
+  const {initialBackgroundColor} = props.userInitialState;
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const [backgroundColor, setBackgroundColor] = useState(initialBackgroundColor || '#f2f2f2');
+
+  const canvasStyles: CSS.Properties = {
+    cursor: isEditMode ? 'pointer' : 'default',
+    aspectRatio: 1,
+    backgroundColor: backgroundColor,
+    display: 'block',
+  }
+
+  // We want to maintain aspect ratio of the thing drawn on the canvas
+  // But since miniapps can be any size, we want to "fill" the excess with
+  // the same color as the background. Sry if this makes no sense.
+  // ...
+  // Also, idk if this is needed w/ background set in styles, maybe I'll revisit
+  const hundredPercentKey = width > height ? 'width' : 'height';
+  canvasStyles[hundredPercentKey] = '100%';
+
+
+  // === Methods to Update Canvas ===
+  // FUTURE: Expose as a customizable callback in props, this is default behavior
+  const clearCanvas = () => {
+    const canvasContext = canvasRef?.current?.getContext('2d');
+    if (!canvasContext) {
+      return;
+    }
+
+    canvasContext.fillStyle = backgroundColor;
+    canvasContext.fillRect(0, 0, width, height);
+  }
+
+  // FUTURE: Expose as a customizable callback in props, takes a canvas context as an arg, can be used to access canvas
+  const draw = () => {
+    clearCanvas();
+  }
+
+  // === Handle Renders and Re-renders ===
+  // Handle drawing on the canvas
+  useEffect(() => {
+    // TODO: Validate?
+    
+  })
+
+  // Update sate variables based on changes to props
+  useEffect(() => {
+    // Update state variables based on changes to props
+    // initialPixels && setPixels(initialPixels);
+    // setShowGrid(shouldShowGridInViewMode || isEditMode);
+    draw();
+  }, [props])
+
+  // === Finally, Return ===
+  return (
+    <canvas
+        ref={canvasRef}
+        width={width + 'px'}
+        height={height + 'px'}
+        style={canvasStyles}
+        // onMouseDown={handleCanvasClick}
+        // onMouseMove={handleCanvasDrag}
+        // onMouseUp={() => setIsMouseDownOnCanvas(false)}
+        onContextMenu={(e) => e.preventDefault()}
+      />
+  )
+}
+
+// stringSizeToNumber takes an input size string like '400px' and returns 400
+function stringSizeToNumber(size: string | undefined): number {
+  if (size === undefined) {
+    // handle edge case, size should never be undefined from Seam render
+    return 400
+  }
+  const regexMatch = size.match(/\d+/)
+  if (!regexMatch) {
+    return 0
+  }
+  return parseInt(regexMatch[0])
+}
+
 export default class WhiteboardBlock extends Block {
-  render() {
+  render(width?: string, height?: string) {
+    const userInitialState = {
+      initialBackgroundColor: '#abc123',
+    }
     return (
-      <h1>Whiteboard Block!</h1>
+      <>
+        <h1>{'wassup'}</h1>
+        <Canvas
+          width={stringSizeToNumber(width)}
+          height={stringSizeToNumber(height)}
+          isEditMode={false}
+          userInitialState={userInitialState}
+        />
+      </>
     );
   }
 
