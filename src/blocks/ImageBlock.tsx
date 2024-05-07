2621773e11b4306f8dae69c658fe8b6241d64ad6
James Buret <james@getseam.xyz>
2024-05-07 02:28:49 -0400

    updated imageblock to accept multiple images and render in a swiper

--- src/blocks/ImageBlock.tsx
+++ src/blocks/ImageBlock.tsx
@@ -1,102 +1,42 @@
-import Block from './Block'
-import { BlockModel } from './types'
+import React from 'react';
+import Block from './Block';
+import { BlockModel } from './types';
 import BlockFactory from './BlockFactory';
-import './BlockStyles.css'
-import Box from "@mui/material/Box";
-import TextField from "@mui/material/TextField";
-import Button from "@mui/material/Button";
-import Typography from "@mui/material/Typography";
-import UploadFormComponent from './utils/UploadFormComponent';
+import './BlockStyles.css';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import ImageUploadPreview from './utils/ImageUploadPreview';
 
 export default class ImageBlock extends Block {
-  render() {
-    if (Object.keys(this.model.data).length === 0) {
-      return BlockFactory.renderEmptyState(this.model, this.onEditCallback!)
+  render(): React.ReactNode {
+    const urls = this.model.data['urls'] ? JSON.parse(this.model.data['urls']) : [];
+  
+    if (urls.length === 0) {
+      return this.renderErrorState();
     }
-
-    let url = this.model.data["url"]
-    if (!url) {
-      return this.renderErrorState()
-    }
-
+  
     return (
-      <img
-        src={url}
-        title="image"
-        style={{
-          height: `100%`,
-          width: `100%`
-        }}
-        onError={(e) => e.currentTarget.src = 'https://www.shutterstock.com/image-illustration/no-picture-available-placeholder-thumbnail-600nw-2179364083.jpg'}
-      />
+      <Swiper spaceBetween={10} slidesPerView={'auto'}>
+        {urls.map((url: any, index: any) => (
+          <SwiperSlide key={index}>
+            <img src={url} style={{ width: "100%", height: "auto" }} />
+          </SwiperSlide>
+        ))}
+      </Swiper>
     );
-  }
+  }  
 
-  addHTTPS(url: string) {
-    return (url.indexOf('://') === -1) ? 'http://' + url : url
-  }
+  renderEditModal(done: (data: BlockModel) => void): React.ReactNode {
+    const initialUrls = this.model.data['urls'] ? JSON.parse(this.model.data['urls']) : [];
 
-  renderEditModal(done: (data: BlockModel) => void) {
-    const onFinish = (event: any) => {
-      event.preventDefault();
-
-      const data = new FormData(event.currentTarget);
-      let url = data.get('url') as string;
-
-      if (!url && this.model.data['url']) {
-        // Assuming that a valid URL is already present (possibly from an uploaded file)
-        url = this.model.data['url'];
-      } else if (url) {
-        // Add 'http://' if necessary
-        url = this.addHTTPS(url);
-      } else {
-        alert('Please enter a valid URL or upload a file before continuing.');
-        return;
-      }
-
-      // Update the model with the new URL
-      this.model.data['url'] = url;
+    const handleUpdate = (urls: string[]) => {
+      this.model.data['urls'] = JSON.stringify(urls);
       done(this.model);
     };
 
-    const uploaderComponent = <UploadFormComponent onUpdate={files => {
-      if (files.length === 0) {
-        console.log('No files selected.');
-        this.model.data['url'] = "";
-      } else {
-        // Directly use the URL from the uploaded file
-        this.model.data['url'] = files[0].fileUrl;
-      }
-    }} />;
-
     return (
-      <>
-        {uploaderComponent}
-        <Typography style={{textAlign: "center", width: "100%", paddingBottom: "10px", paddingTop: "18px"}}>Or</Typography>
-        <Box
-          component="form"
-          onSubmit={onFinish}
-          style={{}}
-        >
-          <TextField
-            margin="normal"
-            defaultValue={this.model.data['url']}
-            fullWidth
-            id="url"
-            label={"Image URL (internet image)"}
-            name="url"
-          />
-          <Button
-            type="submit"
-            variant="contained"
-            className="save-modal-button"
-            sx={{ mt: 3, mb: 2 }}
-          >
-            Preview
-          </Button>
-        </Box>
-      </>
-    )
+      <ImageUploadPreview initialUrls={initialUrls} onUpdate={handleUpdate} />
+    );
   }
 
   renderErrorState() {
@@ -104,8 +44,8 @@ export default class ImageBlock extends Block {
       <img
         src="https://www.shutterstock.com/image-illustration/no-picture-available-placeholder-thumbnail-600nw-2179364083.jpg"
         title="Image"
-        style={{ height: '100%', width: '100%', }}
+        style={{ height: '100%', width: '100%' }}
       />
-    )
+    );
   }
 }
