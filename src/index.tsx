cd402ec53a9c2f6a38c7d3a496a7a050cfabdf0c
Nick Confrey <nickconfrey@earthlink.net>
2024-05-15 15:31:31 -0400

    fix design system

--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,37 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { IonApp, setupIonicReact } from '@ionic/react';
+import { createTheme, StyledEngineProvider } from "@mui/material/styles";
+import { ThemeProvider } from "@mui/styles";
 
 setupIonicReact();
 
+const defaultTheme = createTheme({
+  palette: {
+    primary: {        // Page Background
+      main: "#FEFEFE"
+    },
+    secondary: {
+      main: "#FEFEFE"  // Block Background (Profile, ect. Buttons take on the info)
+    },
+    info: {
+      main: "#0288D1"
+    }
+  },
+  typography: {
+    fontFamily: "Public Sans"
+  }
+});
+
 ReactDOM.render(
   <React.StrictMode>
-      <IonApp className="seam">
-        <App />
-      </IonApp>
+    <IonApp className="seam">
+      <StyledEngineProvider injectFirst>
+        <ThemeProvider theme={defaultTheme}>
+          <App />
+        </ThemeProvider>
+      </StyledEngineProvider>
+    </IonApp>
   </React.StrictMode>,
   document.getElementById("root")
 );
