6267e06d226d6981803ea55c793f283af9a833d8
James Buret <james@getseam.xyz>
2024-05-17 16:26:10 -0400

    fixed ioncontent props for miniapp builder

--- src/App.tsx
+++ src/App.tsx
@@ -64,7 +64,7 @@ export default function App() {
           </div>
         </IonHeader>
       </div>
-      <IonContent>
+      <IonContent fullscreen={true} scrollY={false}>
         <DesktopSidebarWrapper>
           <Feed loadedPosts={loadedPosts} />
         </DesktopSidebarWrapper>
