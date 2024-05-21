d5617cbfd8acae91a05799025648a1933ab4f27b
Nick Confrey <nickconfrey@earthlink.net>
2024-05-21 17:17:25 -0400

    fix padding in composer item

--- src/Composer/ComposerMiniAppItem.js
+++ src/Composer/ComposerMiniAppItem.js
@@ -2,11 +2,11 @@ import ComposerMiniAppPhoto from './ComposerMiniAppPhoto';
 
 function ComposerMiniAppItem({ block, tapAction }) {
   return (
-    <div className="flex flex-row items-start pb-4 pt-4" onClick={() => { tapAction() }}>
+    <div className="flex flex-row items-start" onClick={() => { tapAction() }}>
       <ComposerMiniAppPhoto block={block} size={60} />
       <div className="flex flex-col justify-center items-start">
         <h3 className='align-top'>{block.displayName}</h3>
-        <h4 className="text-[#86868A]">{block.displayDescription}</h4>
+        <h4 className="text-[#86868A] line-clamp-2">{block.displayDescription}</h4>
         <h4 className="mt-2 mb-2 text-gray pt-2">
           Created by: <span className="text-black">{"@" + block.createdBy}</span>
         </h4>
