c30a13195a20e8aa67fc19f1c97ea77124418e65
James Buret <james@getseam.xyz>
2024-05-21 15:52:01 -0400

    truncated miniappitems and made padding per parent

--- src/Composer/ComposerMiniAppItem.js
+++ src/Composer/ComposerMiniAppItem.js
@@ -1,18 +1,21 @@
-import ComposerMiniAppPhoto from './ComposerMiniAppPhoto';
+import React from 'react';
+import ComposerMiniAppPhoto from './ComposerMiniAppPhoto'; // Update the path if necessary
 
 function ComposerMiniAppItem({ block, tapAction }) {
   return (
-    <div className="flex flex-row items-start pb-4 pt-4" onClick={() => { tapAction() }}>
+    <div className="flex flex-row items-start w-full truncate" onClick={tapAction}>
       <ComposerMiniAppPhoto block={block} size={60} />
-      <div className="flex flex-col justify-center items-start">
-        <h3 className='align-top'>{block.displayName}</h3>
-        <h4 className="text-[#86868A]">{block.displayDescription}</h4>
-        <h4 className="mt-2 mb-2 text-gray pt-2">
+      <div className="flex flex-col justify-between items-start ml-4 truncate h-[60px] w-full">
+        <div className="w-full h-full">
+          <h3 className="align-top">{block.displayName}</h3>
+          <h4 className="text-[#86868A] truncate overflow-ellipsis whitespace-nowrap w-auto">{block.displayDescription}</h4>
+        </div>
+        <h4 className="mt-2 mb-2 text-gray">
           Created by: <span className="text-black">{"@" + block.createdBy}</span>
         </h4>
       </div>
     </div>
-  )
+  );
 }
 
 export default ComposerMiniAppItem;
