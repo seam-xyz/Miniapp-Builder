3cab9458599af3054d5bc7b814a519e8e664bf98
James Buret <james@getseam.xyz>
2024-05-22 16:18:18 -0400

    reverted miniapp changes

--- src/Composer/ComposerMiniAppItem.js
+++ src/Composer/ComposerMiniAppItem.js
@@ -1,21 +1,18 @@
-import React from 'react';
-import ComposerMiniAppPhoto from './ComposerMiniAppPhoto'; // Update the path if necessary
+import ComposerMiniAppPhoto from './ComposerMiniAppPhoto';
 
 function ComposerMiniAppItem({ block, tapAction }) {
   return (
-    <div className="flex flex-row items-start w-full truncate" onClick={tapAction}>
+    <div className="flex flex-row items-start" onClick={() => { tapAction() }}>
       <ComposerMiniAppPhoto block={block} size={60} />
-      <div className="flex flex-col justify-between items-start ml-4 truncate h-[60px] w-full">
-        <div className="w-full h-full">
-          <h3 className="align-top">{block.displayName}</h3>
-          <h4 className="text-[#86868A] truncate overflow-ellipsis whitespace-nowrap w-auto">{block.displayDescription}</h4>
-        </div>
-        <h4 className="mt-2 mb-2 text-gray">
+      <div className="flex flex-col justify-center items-start">
+        <h3 className='align-top'>{block.displayName}</h3>
+        <h4 className="text-[#86868A] line-clamp-2">{block.displayDescription}</h4>
+        <h4 className="mt-2 mb-2 text-gray pt-2">
           Created by: <span className="text-black">{"@" + block.createdBy}</span>
         </h4>
       </div>
     </div>
-  );
+  )
 }
 
 export default ComposerMiniAppItem;
