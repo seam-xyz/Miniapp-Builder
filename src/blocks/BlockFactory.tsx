401130056f629e014b5812c4a7d706245bcb76f1
Iyana <114933494+yayakixtenet@users.noreply.github.com>
2024-06-05 13:06:01 -0400

    setup voiceapp block

--- src/blocks/BlockFactory.tsx
+++ src/blocks/BlockFactory.tsx
@@ -1,3 +1,4 @@
+import VoiceApp from './VoiceApp'
 import MoodApp from './MoodApp'
 import BookshelfApp from './BookshelfApp'
 import UnknownApp from './UnknownApp'
@@ -64,6 +65,7 @@ export default class BlockFactory {
       case "Wordle": return new WordleBlock(model, theme)
       case "Mood": return new MoodApp(model, theme)
       case "Whiteboard": return new WhiteboardBlock(model, theme)
+      case "Voice": return new VoiceApp(model, theme)
       // new blocks go here
       default: return new UnknownApp(model, theme)
     }
