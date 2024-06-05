68131dab122352e182d8b4ecd82705a187e9f5f2
SamuelRCrider <sam.crider@aol.com>
2024-06-05 16:47:35 -0400

    organized file

--- src/blocks/VoiceApp.tsx
+++ src/blocks/VoiceApp.tsx
@@ -11,8 +11,74 @@ import GraphicEqIcon from '@mui/icons-material/GraphicEq';
 import MicIcon from '@mui/icons-material/Mic';
 import Fab from '@mui/material/Fab';
 
+/*
+
+  TYPES
+
+*/
+
 type FakeFormProps = {
   onSave: (name: string) => void;
+}
+
+/*
+
+  VoiceApp COMPONENTS
+
+*/
+
+const AudioButtons = () => {
+  return (
+    <Box style={{
+      backgroundColor: 'none', padding: '20px', display: 'flex', justifyContent: 'center', alignItems: 'center', gap: '20px', width: '100%'
+    }} >
+      {/* Microphone button */}
+      <Fab sx={{ width: { xs: "150px", md: "250px", lg: "250px" }, height: { xs: "150px", md: "250px", lg: "250px" } }} style={{
+        backgroundColor: 'black'
+      }}>
+        <MicIcon style={{
+          borderRadius: '50%',
+          color: 'white',
+        }} />
+      </Fab>
+      {/* Post button */}
+      <Fab sx={{
+        width: { xs: "75px", md: "150px", lg: "150px" }, height: { xs: "75px", md: "150px", lg: "150px" }, color: 'white', margin: '10px', padding: '20px'
+      }} style={{
+        backgroundColor: 'red'
+      }}>
+        Post
+      </Fab>
+
+    </Box>
+  )
+}
+
+const AudioCard = () => {
+  return (
+    <Card style={{ backgroundColor: 'black', borderRadius: '30px' }} >
+
+    <CardContent style={{ backgroundColor: 'black', padding: '24px', height: 240, display: 'flex', alignItems: 'center', }}>
+      <Box style={{
+        color: 'black', backgroundColor: 'none', display: 'flex', justifyContent: 'space-between', width: '100%'
+      }}>
+        <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+          <DeleteOutlineIcon style={{ color: 'black', backgroundColor: 'white', borderRadius: '50px' }} />
+          <span style={{ color: 'white' }}>0.00</span>
+        </div>
+        <span style={{ color: 'white' }}>
+          .....<GraphicEqIcon />.....
+        </span>
+        <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }} >
+          < PlayCircleIcon style={{ color: 'white', backgroundColor: 'transparent', borderRadius: '50px' }} />
+          <span style={{ color: 'white' }}>0.00</span>
+
+        </div>
+      </Box>
+    </CardContent>
+
+  </Card>
+)
 }
 
 const FakeForm = ({onSave}: FakeFormProps) => {
@@ -35,7 +101,14 @@ const FakeForm = ({onSave}: FakeFormProps) => {
   
 }
 
+/* 
+
+  SEAM CLASS
+
+*/
+
 export default class VoiceBlock extends Block {
+
   render() {
     return (
       <div>
@@ -45,6 +118,7 @@ export default class VoiceBlock extends Block {
       </div>
     );
   }
+
   renderEditModal(done: (data: BlockModel) => void) {
     const handleSave = (name: string) => {
       this.model.data["name"] = name
@@ -53,51 +127,9 @@ export default class VoiceBlock extends Block {
 
     return (
       <>
-      <FakeForm onSave={handleSave}/>
-        <Card style={{ backgroundColor: 'black', borderRadius: '30px' }} >
-
-          <CardContent style={{ backgroundColor: 'black', padding: '24px', height: 240, display: 'flex', alignItems: 'center', }}>
-            <Box style={{
-              color: 'black', backgroundColor: 'none', display: 'flex', justifyContent: 'space-between', width: '100%'
-            }}>
-              <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
-                <DeleteOutlineIcon style={{ color: 'black', backgroundColor: 'white', borderRadius: '50px' }} />
-                <span style={{ color: 'white' }}>0.00</span>
-              </div>
-              <span style={{ color: 'white' }}>
-                .....<GraphicEqIcon />.....
-              </span>
-              <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }} >
-                < PlayCircleIcon style={{ color: 'white', backgroundColor: 'transparent', borderRadius: '50px' }} />
-                <span style={{ color: 'white' }}>0.00</span>
-
-              </div>
-            </Box>
-          </CardContent>
-
-        </Card>
-        <Box style={{
-          backgroundColor: 'none', padding: '20px', display: 'flex', justifyContent: 'center', alignItems: 'center', gap: '20px', width: '100%'
-        }} >
-          {/* Microphone button */}
-          <Fab sx={{ width: { xs: "150px", md: "250px", lg: "250px" }, height: { xs: "150px", md: "250px", lg: "250px" } }} style={{
-            backgroundColor: 'black'
-          }}>
-            <MicIcon style={{
-              borderRadius: '50%',
-              color: 'white',
-            }} />
-          </Fab>
-          {/* Post button */}
-          <Fab sx={{
-            width: { xs: "75px", md: "150px", lg: "150px" }, height: { xs: "75px", md: "150px", lg: "150px" }, color: 'white', margin: '10px', padding: '20px'
-          }} style={{
-            backgroundColor: 'red'
-          }}>
-            Post
-          </Fab>
-
-        </Box>
+        <FakeForm onSave={handleSave}/>
+        <AudioCard />
+        <AudioButtons />
       </>
 
     )
