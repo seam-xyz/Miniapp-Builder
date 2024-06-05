4bd5026d50e610dd854f791159c9ee1884001622
SamuelRCrider <sam.crider@aol.com>
2024-06-05 15:38:37 -0400

    tested out functionality

--- src/blocks/VoiceApp.tsx
+++ src/blocks/VoiceApp.tsx
@@ -2,18 +2,50 @@ import Block from './Block'
 import { BlockModel } from './types'
 import BlockFactory from './BlockFactory';
 import './BlockStyles.css'
+import { useState } from 'react';
+import Button from '@mui/material/Button';
+
+type FakeFormProps = {
+  onSave: (name: string) => void;
+}
+
+const FakeForm = ({onSave}: FakeFormProps) => {
+  const [name, setName] = useState<string>()
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    if (name) {
+     onSave(name)
+     
+    }
+  }
+  return (
+    <div>
+    <form onSubmit={handleSubmit}>
+      <input style={{border: "1px solid"}} type="text" value={name} onChange={(e) => setName(e.target.value)}  />
+      <button type='submit'>POST</button>
+    </form>
+    </div>
+  )
+  
+}
 
 export default class VoiceBlock extends Block {
   render() {
     return (
-      <h1>Voice Block!</h1>
+      <div>
+        <div>{this.model.data['']}</div>
+        <div></div>
+        <div></div>
+      </div>
     );
   }
 
   renderEditModal(done: (data: BlockModel) => void) {
-    return (
-      <h1>Edit Voice Block!</h1>
-    )
+    const handleSave = (name: string) => {
+      this.model.data["name"] = name
+      done(this.model)
+    }
+   return <FakeForm onSave={handleSave}/>
   }
 
   renderErrorState() {
