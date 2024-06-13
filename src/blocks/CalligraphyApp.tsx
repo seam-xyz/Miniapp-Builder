313a5cbb8e06600d6a9a290e9d61a22869ed7015
Steven <ssebexen@gmail.com>
2024-06-13 16:08:50 -0400

    Added toolbar

--- src/blocks/CalligraphyApp.tsx
+++ src/blocks/CalligraphyApp.tsx
@@ -1,10 +1,13 @@
 import Block from './Block'
 import { BlockModel } from './types'
-import BlockFactory from './BlockFactory';
 import './BlockStyles.css'
 import SeamSaveButton from '../components/SeamSaveButton';
 import { useEffect, useRef } from 'react';
-import { JsxElement } from 'typescript';
+
+import EditIcon from '@mui/icons-material/Edit';
+import BorderOuterIcon from '@mui/icons-material/BorderOuter';
+import UndoIcon from '@mui/icons-material/Undo';
+import CloseIcon from '@mui/icons-material/Close';
 
 interface CalligraphyCanvasProps {
 
@@ -36,8 +39,8 @@ interface ColorSwatchProps {
 const ColorSwatch = (props: ColorSwatchProps) => {
   return (
     <div className='flex flex-0 basis-[14.285714285%] justify-center'>
-      <div className='flex flex-0 place-items-center place-content-center w-8 h-8 rounded-full bg-[#ededed]'>
-        <div className='w-6 h-6 rounded-full border border-[#ffffff]' style={{ backgroundColor: props.color }}>
+      <div className='flex flex-0 place-items-center place-content-center w-12 h-12 rounded-full bg-[#ededed]'>
+        <div className='w-8 h-8 rounded-full border border-white' style={{ backgroundColor: props.color }}>
           
         </div>
       </div>
@@ -50,20 +53,28 @@ interface CalligraphyPaletteProps {
 }
 const CalligraphyPalette = (props: CalligraphyPaletteProps) => {
   return (
-    <div className='flex flex-row flex-wrap py-2 border-2 rounded-md bg-[#fafafa] justify-start'>
+    <div className='flex flex-row flex-wrap gap-2 py-2 border-2 rounded-md bg-[#fbfbfb] justify-start'>
       {props.colors.map (color => <ColorSwatch key={color} color={color} />)}
     </div>
   )
 }
 
-const CalligraphyToolbar = () => {
+interface CalligraphyToolbarProps {
+  currentColor: string
+}
+const CalligraphyToolbar = (props: CalligraphyToolbarProps) => {
   return (
     <div className='flex justify-between'>
-      <div>
-        Tab selector
+      <div className='flex gap-4 border-2 rounded-full p-4 bg-[#fbfbfb]'>
+        <div className='flex flex-0 w-10 h-10 rounded-full bg-[#ededed] place-items-center place-content-center'>
+          <div className='w-8 h-8 rounded-full border border-white' style={{ backgroundColor: props.currentColor }} />
+        </div>
+        <div className='flex flex-0 w-10 h-10 rounded-full bg-[#ededed] place-items-center place-content-center'><EditIcon /></div>
+        <div className='flex flex-0 w-10 h-10 rounded-full bg-[#ededed] place-items-center place-content-center'><BorderOuterIcon /></div>
       </div>
-      <div>
-        Undo/Clear
+      <div className='flex gap-4 border-2 rounded-full p-4 bg-[#fbfbfb]'>
+        <div className='flex flex-0 w-10 h-10 rounded-full bg-[#ededed] place-items-center place-content-center'><UndoIcon /></div>
+        <div className='flex flex-0 w-10 h-10 rounded-full bg-[#ededed] place-items-center place-content-center'><CloseIcon /></div>
       </div>
     </div>
   )
@@ -82,10 +93,11 @@ export default class CalligraphyBlock extends Block {
     return (
       <div>
         <h1>Edit Calligraphy Block!</h1>
-
-        <CalligraphyCanvas/>
-        <CalligraphyPalette colors={[ '#cdb4db', '#ffc8ddff', '#ffafccff', '#bde0feff', '#a2d2ffff' ]} />
-        <CalligraphyToolbar/>
+        <div className='flex flex-col gap-6'>
+          <CalligraphyCanvas/>
+          <CalligraphyPalette colors={[ '#cdb4db', '#ffc8ddff', '#ffafccff', '#bde0feff', '#a2d2ffff', '#264653', '#2A9D8F', '#E9C46A', '#F4A261', '#E76F51', ]} />
+          <CalligraphyToolbar currentColor='#cdb4db'/>
+        </div>
         <div className='absolute bottom-0 left-0 right-0 p-4'>
           <SeamSaveButton onClick={onSave}/>
         </div>
