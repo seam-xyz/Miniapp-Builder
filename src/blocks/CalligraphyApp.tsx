2e20475fd0768c8156b29a2b31fc51a29d0434d4
Steven <ssebexen@gmail.com>
2024-06-13 16:39:17 -0400

    Added color selection

--- src/blocks/CalligraphyApp.tsx
+++ src/blocks/CalligraphyApp.tsx
@@ -2,7 +2,7 @@ import Block from './Block'
 import { BlockModel } from './types'
 import './BlockStyles.css'
 import SeamSaveButton from '../components/SeamSaveButton';
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 import EditIcon from '@mui/icons-material/Edit';
 import BorderOuterIcon from '@mui/icons-material/BorderOuter';
@@ -35,11 +35,16 @@ const CalligraphyCanvas = (props: CalligraphyCanvasProps) => {
 
 interface ColorSwatchProps {
   color: string
+  activeColor: string
+  onClick: () => void
 }
 const ColorSwatch = (props: ColorSwatchProps) => {
   return (
     <div className='flex flex-0 basis-[14.285714285%] justify-center'>
-      <div className='flex flex-0 place-items-center place-content-center w-12 h-12 rounded-full bg-[#ededed]'>
+      <div className='flex flex-0 place-items-center place-content-center w-10 h-10 rounded-full bg-[#ededed] border-fuchsia-500'
+        style={{ borderWidth : props.color === props.activeColor ? '2px' : '0px' }}
+        onClick={props.onClick}
+      >
         <div className='w-8 h-8 rounded-full border border-white' style={{ backgroundColor: props.color }}>
           
         </div>
@@ -49,25 +54,29 @@ const ColorSwatch = (props: ColorSwatchProps) => {
 }
 
 interface CalligraphyPaletteProps {
+  activeColor: string
   colors: string[]
+  onColorSelected: (color: string) => void
 }
 const CalligraphyPalette = (props: CalligraphyPaletteProps) => {
   return (
     <div className='flex flex-row flex-wrap gap-2 py-2 border-2 rounded-md bg-[#fbfbfb] justify-start'>
-      {props.colors.map (color => <ColorSwatch key={color} color={color} />)}
+      {props.colors.map (color =>
+        <ColorSwatch key={color} color={color} onClick={() => props.onColorSelected(color)} activeColor={props.activeColor} />
+      )}
     </div>
   )
 }
 
 interface CalligraphyToolbarProps {
-  currentColor: string
+  activeColor: string
 }
 const CalligraphyToolbar = (props: CalligraphyToolbarProps) => {
   return (
     <div className='flex justify-between'>
       <div className='flex gap-4 border-2 rounded-full p-4 bg-[#fbfbfb]'>
         <div className='flex flex-0 w-10 h-10 rounded-full bg-[#ededed] place-items-center place-content-center'>
-          <div className='w-8 h-8 rounded-full border border-white' style={{ backgroundColor: props.currentColor }} />
+          <div className='w-8 h-8 rounded-full border border-white' style={{ backgroundColor: props.activeColor }} />
         </div>
         <div className='flex flex-0 w-10 h-10 rounded-full bg-[#ededed] place-items-center place-content-center'><EditIcon /></div>
         <div className='flex flex-0 w-10 h-10 rounded-full bg-[#ededed] place-items-center place-content-center'><BorderOuterIcon /></div>
@@ -80,7 +89,32 @@ const CalligraphyToolbar = (props: CalligraphyToolbarProps) => {
   )
 }
 
+interface CalligraphyEditProps {
+  onSave: () => void
+}
+const CalligraphyEdit = (props: CalligraphyEditProps) => {
+  const [activeColor, setActiveColor] = useState('#cdb4db');
+
+  return (
+    <div>
+      <h1>Edit Calligraphy Block!</h1>
+      <div className='flex flex-col gap-6'>
+        <CalligraphyCanvas/>
+        <CalligraphyPalette
+          colors={[ '#cdb4db', '#ffc8ddff', '#ffafccff', '#bde0feff', '#a2d2ffff', '#264653', '#2A9D8F', '#E9C46A', '#F4A261', '#E76F51', ]}
+          onColorSelected={color => setActiveColor(color)}
+          activeColor={activeColor} />
+        <CalligraphyToolbar activeColor={activeColor}/>
+      </div>
+      <div className='absolute bottom-0 left-0 right-0 p-4'>
+        <SeamSaveButton onClick={props.onSave}/>
+      </div>
+    </div>
+  )
+}
+
 export default class CalligraphyBlock extends Block {
+
   render () {
     return (
       <h1>Calligraphy Block!</h1>
@@ -91,17 +125,7 @@ export default class CalligraphyBlock extends Block {
   renderEditModal(done: (data: BlockModel) => void) {
     const onSave = () => done(this.model)
     return (
-      <div>
-        <h1>Edit Calligraphy Block!</h1>
-        <div className='flex flex-col gap-6'>
-          <CalligraphyCanvas/>
-          <CalligraphyPalette colors={[ '#cdb4db', '#ffc8ddff', '#ffafccff', '#bde0feff', '#a2d2ffff', '#264653', '#2A9D8F', '#E9C46A', '#F4A261', '#E76F51', ]} />
-          <CalligraphyToolbar currentColor='#cdb4db'/>
-        </div>
-        <div className='absolute bottom-0 left-0 right-0 p-4'>
-          <SeamSaveButton onClick={onSave}/>
-        </div>
-      </div>
+      <CalligraphyEdit onSave={onSave} />
     )
   }
 
