978e2c3ba95dc473492a0801d59967d83b505b79
Nick Confrey <nickconfrey@earthlink.net>
2024-06-27 11:04:39 -0400

    prevent crashes on undefined new miniapp types

--- src/Composer/ComposerMiniAppPhoto.js
+++ src/Composer/ComposerMiniAppPhoto.js
@@ -3,7 +3,7 @@ import { Avatar } from '@mui/material';
 function ComposerMiniAppPhoto({ block, size }) {
   return (
     <Avatar style={{ borderRadius: 5, backgroundColor: "gray", height: size, width: size, marginRight: 16 }}>
-      <img src={block.icon} style={{ height: size }} />
+      <img src={block?.icon} style={{ height: size }} />
     </Avatar>
   )
 }
