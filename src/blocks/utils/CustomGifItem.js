9ffe509341ecb551502b340fb1e53c1a993b1490
James Buret <james@getseam.xyz>
2024-06-28 11:38:09 -0400

    removed unused code

--- src/blocks/utils/CustomGifItem.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function CustomGifItem({ gif, onGifClick }) {
-  return (
-    <div className="p-1 cursor-pointer" onClick={() => onGifClick(gif)}>
-      <img src={gif.images.fixed_width.url} alt={gif.title} className="rounded-lg" />
-    </div>
-  );
-}
