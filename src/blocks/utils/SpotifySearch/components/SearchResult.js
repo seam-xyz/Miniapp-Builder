fb9d3f3818813da86b9995a346151d990a4c970f
James Buret <james@getseam.xyz>
2024-07-08 16:52:34 -0400

    simplified prop passing for searchresult component

--- src/blocks/utils/SpotifySearch/components/SearchResult.js
+++ src/blocks/utils/SpotifySearch/components/SearchResult.js
@@ -1,18 +1,10 @@
 import React from 'react';
 import TracksList from './TracksList';
 
-const SearchResult = (props) => {
-  const {
-    onChooseTrack,
-    tracks,
-    selectedTrack,
-  } = props;
-
-  return (
-    <>
-      {tracks && <TracksList onChooseTrack={onChooseTrack} tracks={tracks} selectedTrack={selectedTrack} />}
-    </>
-  );
-};
+const SearchResult = ({ onChooseTrack, tracks, selectedTrack }) => (
+  <>
+    {tracks && <TracksList onChooseTrack={onChooseTrack} tracks={tracks} selectedTrack={selectedTrack} />}
+  </>
+);
 
 export default SearchResult;
