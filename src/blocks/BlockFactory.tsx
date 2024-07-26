46bffd876ce1a4820fe5181173a82ece8c31ad19
Nick Confrey <nickconfrey@earthlink.net>
2024-07-26 12:15:51 -0400

    wire up all miniapps in the factory

--- src/blocks/BlockFactory.tsx
+++ src/blocks/BlockFactory.tsx
@@ -1,116 +1,113 @@
-// import { ImagePuzzleFeedComponent, ImagePuzzleComposerComponent } from './ImagePuzzleBlock'
-// import { LocalelocatrFeedComponent, LocalelocatrComposerComponent } from './LocalelocatrBlock'
-// import { MapFeedComponent, MapComposerComponent } from './MapBlock'
-// import { CameraFeedComponent, CameraComposerComponent } from './CameraBlock'
-// import { CalligraphyFeedComponent, CalligraphyComposerComponent } from './CalligraphyBlock'
-// import { MoodFeedComponent, MoodComposerComponent } from './MoodBlock'
+import { ImagePuzzleFeedComponent, ImagePuzzleComposerComponent } from './ImagePuzzleBlock'
+import { LocalelocatrFeedComponent, LocalelocatrComposerComponent } from './localelocatrBlock'
+import { MapFeedComponent, MapComposerComponent } from './MapApp'
+import { CameraFeedComponent, CameraComposerComponent } from './CameraApp'
+import { CalligraphyFeedComponent, CalligraphyComposerComponent } from './CalligraphyApp'
+import { MoodFeedComponent, MoodComposerComponent } from './MoodApp'
 import { BookshelfFeedComponent, BookshelfComposerComponent } from './BookshelfApp'
 import { UnknownFeedComponent, UnknownComposerComponent } from './UnknownApp'
-// import { EyesFeedComponent, EyesComposerComponent } from './EyesBlock'
-// import { WhiteboardFeedComponent, WhiteboardComposerComponent } from './WhiteboardBlock'
-// import { FcUserFeedComponent, FcUserComposerComponent } from './FcUserFeedBlock'
-// import { TokenHoldingsFeedComponent, TokenHoldingsComposerComponent } from './TokenHoldingsBlock'
-// import { NFTFeedComponent, NFTComposerComponent } from './NFTBlock'
-// import { PhotoAlbumFeedComponent, PhotoAlbumComposerComponent } from './PhotoAlbumBlock'
-// import { MarqueeFeedComponent, MarqueeComposerComponent } from './MarqueeBlock'
-// import { PokemonFeedComponent, PokemonComposerComponent } from './PokemonBlock'
-// import { NFTsFeedComponent, NFTsComposerComponent } from './NFTsBlock'
-// import { PixelArtFeedComponent, PixelArtComposerComponent } from './PixelArtBlock'
-// import { MusicFeedComponent, MusicComposerComponent } from './MusicBlock'
-// import { IFrameFeedComponent, IFrameComposerComponent } from './IFrameBlock'
-// import { TextEditFeedComponent, TextEditComposerComponent } from './TextEditBlock'
-// import { LinkFeedComponent, LinkComposerComponent } from './LinkBlock'
-// import { ImageFeedComponent, ImageComposerComponent } from './ImageBlock'
-// import { TwitterFeedComponent, TwitterComposerComponent } from './TwitterBlock'
-// import { TweetFeedComponent, TweetComposerComponent } from './TweetBlock'
-// import { EmptyFeedComponent, EmptyComposerComponent } from './EmptyBlock'
-// import { ProfileFeedComponent, ProfileComposerComponent } from './ProfileBlock'
-// import { GiphyFeedComponent, GiphyComposerComponent } from './GiphyBlock'
-// import { FlashingTextFeedComponent, FlashingTextComposerComponent } from './FlashingTextBlock'
-// import { CountdownFeedComponent, CountdownComposerComponent } from './CountdownBlock'
-// import { LinkBookmarkFeedComponent, LinkBookmarkComposerComponent } from './LinkBookmarkBlock'
-// import { VideoFeedComponent, VideoComposerComponent } from './VideoBlock'
-// import { MondrianFeedComponent, MondrianComposerComponent } from './MondrianBlock'
-// import { WordleFeedComponent, WordleComposerComponent } from './WordleBlock'
+import { EyesFeedComponent, EyesComposerComponent } from './eyesBlock'
+import { WhiteboardFeedComponent, WhiteboardComposerComponent } from './WhiteboardBlock'
+import { FcUserFeedComponent, FcUserComposerComponent } from './fcUserFeedBlock'
+import { TokenHoldingsFeedComponent, TokenHoldingsComposerComponent } from './tokenHoldingsBlock'
+import { NFTFeedComponent, NFTComposerComponent } from './NFTBlock'
+import { PhotoAlbumFeedComponent, PhotoAlbumComposerComponent } from './PhotoAlbumBlock'
+import { MarqueeFeedComponent, MarqueeComposerComponent } from './MarqueeBlock'
+import { PokemonFeedComponent, PokemonComposerComponent } from './PokemonBlock'
+import { NFTsFeedComponent, NFTsComposerComponent } from './NFTsBlock'
+import { PixelArtFeedComponent, PixelArtComposerComponent } from './PixelArtBlock'
+import { MusicFeedComponent, MusicComposerComponent } from './MusicBlock'
+import { IFrameFeedComponent, IFrameComposerComponent } from './IFrameBlock'
+import { TextEditFeedComponent, TextEditComposerComponent } from './TextEditBlock'
+import { LinkFeedComponent, LinkComposerComponent } from './LinkBlock'
+import { ImageFeedComponent, ImageComposerComponent } from './ImageBlock'
+import { TwitterFeedComponent, TwitterComposerComponent } from './TwitterBlock'
+import { TweetFeedComponent, TweetComposerComponent } from './TweetBlock'
+import { ProfileFeedComponent, ProfileComposerComponent } from './ProfileBlock'
+import { GiphyFeedComponent, GiphyComposerComponent } from './GiphyBlock'
+import { FlashingTextFeedComponent, FlashingTextComposerComponent } from './FlashingTextBlock'
+import { CountdownFeedComponent, CountdownComposerComponent } from './CountdownBlock'
+import { LinkBookmarkFeedComponent, LinkBookmarkComposerComponent } from './LinkBookmarkBlock'
+import { VideoFeedComponent, VideoComposerComponent } from './VideoBlock'
+import { MondrianFeedComponent, MondrianComposerComponent } from './MondrianBlock'
+import { WordleFeedComponent, WordleComposerComponent } from './WordleBlock'
 import { BlockModel, BlockTypes, ComposerComponentProps } from './types'
 
 export default class BlockFactory {
   static getFeedComponent(model: BlockModel) {
     switch (model.type) {
-      // case "iframe": return IFrameFeedComponent;
-      // case "link": return LinkFeedComponent;
-      // case "image": return ImageFeedComponent;
-      // case "twitter": return TwitterFeedComponent;
-      // case "text": return TextFeedComponent;
-      // case "tweet": return TweetFeedComponent;
-      // case "Link Bookmark": return LinkBookmarkFeedComponent;
-      // case "Music": return MusicFeedComponent;
-      // case "video": return VideoFeedComponent;
-      // case "profile": return ProfileFeedComponent;
-      // case "giphy": return GiphyFeedComponent;
-      // case "PixelArt": return PixelArtFeedComponent;
-      // case "NFTs": return NFTsFeedComponent;
-      // case "Pokemon": return PokemonFeedComponent;
-      // case "Marquee": return MarqueeFeedComponent;
-      // case "PhotoAlbum": return PhotoAlbumFeedComponent;
-      // case "FlashingText": return FlashingTextFeedComponent;
-      // case "tokenHoldings": return tokenHoldingsFeedComponent;
-      // case "NFT": return NFTFeedComponent;
-      // case "countdown": return CountdownFeedComponent;
-      // case "fcUserFeed": return fcUserFeedFeedComponent;
-      // case "eyes": return eyesFeedComponent;
-      // case "Mondrian": return MondrianFeedComponent;
+      case "iframe": return <IFrameFeedComponent model={model} />;
+      case "link": return <LinkFeedComponent model={model} />;
+      case "image": return <ImageFeedComponent model={model} />;
+      case "twitter": return <TwitterFeedComponent model={model} />;
+      case "text": return <TextEditFeedComponent model={model} />;
+      case "tweet": return <TweetFeedComponent model={model} />;
+      case "Link Bookmark": return <LinkBookmarkFeedComponent model={model} />;
+      case "Music": return <MusicFeedComponent model={model} />;
+      case "video": return <VideoFeedComponent model={model} />;
+      case "profile": return <ProfileFeedComponent model={model} />;
+      case "giphy": return <GiphyFeedComponent model={model} />;
+      case "PixelArt": return <PixelArtFeedComponent model={model} />;
+      case "NFTs": return <NFTsFeedComponent model={model} />;
+      case "Pokemon": return <PokemonFeedComponent model={model} />;
+      case "Marquee": return <MarqueeFeedComponent model={model} />;
+      case "PhotoAlbum": return <PhotoAlbumFeedComponent model={model} />;
+      case "FlashingText": return <FlashingTextFeedComponent model={model} />;
+      case "tokenHoldings": return <TokenHoldingsFeedComponent model={model} />;
+      case "NFT": return <NFTFeedComponent model={model} />;
+      case "countdown": return <CountdownFeedComponent model={model} />;
+      case "fcUserFeed": return <FcUserFeedComponent model={model} />;
+      case "eyes": return <EyesFeedComponent model={model} />;
+      case "Mondrian": return <MondrianFeedComponent model={model} />;
       case "Bookshelf": return <BookshelfFeedComponent model={model} />;
-      // case "Wordle": return WordleFeedComponent;
-      // case "Mood": return MoodFeedComponent;
-      // case "Whiteboard": return WhiteboardFeedComponent;
-      // case "ImagePuzzle": return ImagePuzzleFeedComponent;
-      // case "Camera": return CameraFeedComponent;
-      // case "Map": return MapFeedComponent;
-      // case "Calligraphy": return CalligraphyFeedComponent;
-      // case "localelocatr": return localelocatrFeedComponent;
+      case "Wordle": return <WordleFeedComponent model={model} />;
+      case "Mood": return <MoodFeedComponent model={model} />;
+      case "Whiteboard": return <WhiteboardFeedComponent model={model} />;
+      case "ImagePuzzle": return <ImagePuzzleFeedComponent model={model} />;
+      case "Camera": return <CameraFeedComponent model={model} />;
+      case "Map": return <MapFeedComponent model={model} />;
+      case "Calligraphy": return <CalligraphyFeedComponent model={model} />;
+      case "localelocatr": return <LocalelocatrFeedComponent model={model} />;
       // new feed components go here
       default: return UnknownFeedComponent({ model });
     }
   }
 
   static getComposerComponent(props: ComposerComponentProps) {
-    console.log(props)
     const { model } = props;
-    console.log(model)
     switch (model.type) {
-      // case "iframe": return IFrameComposerComponent;
-      // case "link": return LinkComposerComponent;
-      // case "image": return ImageComposerComponent;
-      // case "twitter": return TwitterComposerComponent;
-      // case "text": return TextComposerComponent;
-      // case "tweet": return TweetComposerComponent;
-      // case "Link Bookmark": return LinkBookmarkComposerComponent;
-      // case "Music": return MusicComposerComponent;
-      // case "video": return VideoComposerComponent;
-      // case "profile": return ProfileComposerComponent;
-      // case "giphy": return GiphyComposerComponent;
-      // case "PixelArt": return PixelArtComposerComponent;
-      // case "NFTs": return NFTsComposerComponent;
-      // case "Pokemon": return PokemonComposerComponent;
-      // case "Marquee": return MarqueeComposerComponent;
-      // case "PhotoAlbum": return PhotoAlbumComposerComponent;
-      // case "FlashingText": return FlashingTextComposerComponent;
-      // case "tokenHoldings": return tokenHoldingsComposerComponent;
-      // case "NFT": return NFTComposerComponent;
-      // case "countdown": return CountdownComposerComponent;
-      // case "fcUserFeed": return fcUserFeedComposerComponent;
-      // case "eyes": return eyesComposerComponent;
-      // case "Mondrian": return MondrianComposerComponent;
+      case "iframe": return IFrameComposerComponent(props);
+      case "link": return LinkComposerComponent(props);
+      case "image": return ImageComposerComponent(props);
+      case "twitter": return TwitterComposerComponent(props);
+      case "text": return TextEditComposerComponent(props);
+      case "tweet": return TweetComposerComponent(props);
+      case "Link Bookmark": return LinkBookmarkComposerComponent(props);
+      case "Music": return MusicComposerComponent(props);
+      case "video": return VideoComposerComponent(props);
+      case "profile": return ProfileComposerComponent(props);
+      case "giphy": return GiphyComposerComponent(props);
+      case "PixelArt": return PixelArtComposerComponent(props);
+      case "NFTs": return NFTsComposerComponent(props);
+      case "Pokemon": return PokemonComposerComponent(props);
+      case "Marquee": return MarqueeComposerComponent(props);
+      case "PhotoAlbum": return PhotoAlbumComposerComponent(props);
+      case "FlashingText": return FlashingTextComposerComponent(props);
+      case "tokenHoldings": return TokenHoldingsComposerComponent(props);
+      case "NFT": return NFTComposerComponent(props);
+      case "countdown": return CountdownComposerComponent(props);
+      case "fcUserFeed": return FcUserComposerComponent(props);
+      case "eyes": return EyesComposerComponent(props);
+      case "Mondrian": return MondrianComposerComponent(props);
       case "Bookshelf": return BookshelfComposerComponent(props);
-      // case "Wordle": return WordleComposerComponent;
-      // case "Mood": return MoodComposerComponent;
-      // case "Whiteboard": return WhiteboardComposerComponent;
-      // case "ImagePuzzle": return ImagePuzzleComposerComponent;
-      // case "Camera": return CameraComposerComponent;
-      // case "Map": return MapComposerComponent;
-      // case "Calligraphy": return CalligraphyComposerComponent;
-      // case "localelocatr": return localelocatrComposerComponent;
+      case "Wordle": return WordleComposerComponent(props);
+      case "Mood": return MoodComposerComponent(props);
+      case "Whiteboard": return WhiteboardComposerComponent(props);
+      case "ImagePuzzle": return ImagePuzzleComposerComponent(props);
+      case "Camera": return CameraComposerComponent(props);
+      case "Map": return MapComposerComponent(props);
+      case "Calligraphy": return CalligraphyComposerComponent(props);
+      case "localelocatr": return LocalelocatrComposerComponent(props);
       // new composer components go here
       default: return UnknownComposerComponent(props);
     }
