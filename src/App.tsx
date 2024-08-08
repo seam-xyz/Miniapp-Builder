cc3f29504eaf7e452c0b2d3a06f2ff988525566e
James Buret <james@getseam.xyz>
2024-08-08 14:49:52 -0400

    fixed account prop for default post

--- src/App.tsx
+++ src/App.tsx
@@ -45,7 +45,7 @@ export default function App() {
   const [loadedPosts, setLoadedPosts] = useState<BlockModel[]>([
     {
       type: 'FlashingText',
-      account: {id: "", username: ""},
+      account: {},
       data: {
         text: 'Welcome to the Seam Miniapp Builder!',
       },
