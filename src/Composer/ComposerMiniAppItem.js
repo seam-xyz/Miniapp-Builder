a2a3da3700d8ec9090be8fad7cefb869f9345a2d
Nick Confrey <nickconfrey@earthlink.net>
2024-08-14 12:26:47 -0400

    multiple creators

--- src/Composer/ComposerMiniAppItem.js
+++ src/Composer/ComposerMiniAppItem.js
@@ -1,6 +1,7 @@
 import ComposerMiniAppPhoto from './ComposerMiniAppPhoto';
 
 function ComposerMiniAppItem({ block, tapAction }) {
+  const createdBy = block.createdBy.map((split) => "@" + split.username).join(", ");
   return (
     <div className="flex flex-row items-start" onClick={() => { tapAction() }}>
       <ComposerMiniAppPhoto block={block} size={60} />
@@ -8,7 +9,7 @@ function ComposerMiniAppItem({ block, tapAction }) {
         <h3 className='align-top'>{block?.displayName ?? "New Miniapp"}</h3>
         <h4 className="text-[#86868A] line-clamp-2">{block?.displayDescription ?? "Update your app to use the new miniapp!"}</h4>
         <h4 className="mt-2 mb-2 text-gray pt-2">
-          Created by: <span className="text-black">{"@" + block?.createdBy ?? ""}</span>
+          Created by: <span className="text-black">{createdBy}</span>
         </h4>
       </div>
     </div>
