46be02d92ac1c3c1d5fd2d4e7d5ae2c5b94f68b5
James Buret <james@getseam.xyz>
2024-08-20 15:20:02 -0400

    changed styling to be a column layout

--- src/blocks/WardrobeApp.tsx
+++ src/blocks/WardrobeApp.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import Box from '@mui/material/Box';
 import Tab from '@mui/material/Tab';
 import Tabs from '@mui/material/Tabs';
-import { Button } from '@mui/material';
+import { Avatar, Button } from '@mui/material';
 import Slider from '@mui/material/Slider';
 import ImageList from '@mui/material/ImageList';
 import Typography from '@mui/material/Typography';
@@ -44,6 +44,35 @@ export const WardrobeComposerComponent = ({ model, done }: ComposerComponentProp
   });
   const [selectColor, setColor] = useState(model.data.BgColor ? model.data.BgColor : "bg-white");
 
+  const backgroundColors = [
+    'bg-white',
+    'bg-slate',
+    'bg-gray',
+    'bg-zinc',
+    'bg-neutral',
+    'bg-stone',
+    'bg-red',
+    'bg-orange',
+    'bg-amber',
+    'bg-yellow',
+    'bg-lime',
+    'bg-green',
+    'bg-emerald',
+    'bg-teal',
+    'bg-cyan',
+    'bg-sky',
+    'bg-blue',
+    'bg-indigo',
+    'bg-violet',
+    'bg-purple',
+    'bg-fuchsia',
+    'bg-pink',
+    'bg-rose',
+  ];
+
+  const [backgroundColor, setBackgroundColor] = useState(selectColor === "bg-white" ? selectColor : selectColor.slice(0, -4));
+  const [colorLevel, setColorLevel] = useState(selectColor === "bg-white" ? "-500" : selectColor.slice(-4, selectColor.length));
+
   const handleDressUp = (part: string, outfit: string) => {
     setSelectedOutfits((prevOutfits: any) => ({
       ...prevOutfits,
@@ -51,28 +80,59 @@ export const WardrobeComposerComponent = ({ model, done }: ComposerComponentProp
     }));
   };
 
+  const handleIntensityChange = (event: Event, newValue: number | number[]) => {
+    setColorLevel("-" + (newValue as number).toString());
+    setColor(backgroundColor + "-" + (newValue as number).toString());
+  };
+
+  const handleColorChange = (newValue: string) => {
+    setBackgroundColor(newValue as string);
+    setColor((newValue as string) + colorLevel);
+  };
+
   const handleSubmit = () => {
-    // console.log('----' + selectColor)
     model.data.Outfits = JSON.stringify(selectedOutfits);
     model.data.BgColor = selectColor;
     done(model);
   }
 
   return (
-    <div>
-      <div className="flex justify-between gap-2 select-none">
-        <DressUpDisplay outfits={selectedOutfits} backColor={selectColor} setBackColor={setColor} />
-        <MenuDisplay onDressUp={handleDressUp} />
-        <Button sx={{position: "absolute", bottom: 50, right: 40}} color="secondary" variant="contained" onClick={handleSubmit}>
-          <span>Preview</span>
-          <ChevronRightRoundedIcon className="-translate-y-[1px]" />
-        </Button>
+    <div className="flex flex-col h-screen w-full select-none">
+      <DressUpDisplay outfits={selectedOutfits} backColor={selectColor} setBackColor={setColor} />
+      <div className="flex flex-col items-center w-full h-full overflow-y-visible overflow-x-hidden">
+        <Box className="relative flex flex-col items-center w-full p-4 bg-transparent rounded-lg">
+          <MenuDisplay onDressUp={handleDressUp} />
+          <Slider
+            aria-label="Color Intensity"
+            defaultValue={500}
+            onChange={handleIntensityChange}
+            step={100}
+            marks
+            min={100}
+            max={900}
+            color="secondary"
+            valueLabelDisplay="off"
+            className="mt-4"
+          />
+          <Typography className="text-[#9c27b0] mt-2">Change Intensity</Typography>
+          <Box className="grid grid-cols-11 border p-2 bg-slate-200 rounded-md mt-4">
+            {backgroundColors.map((color, index) => (
+              <button
+                key={index}
+                className={`w-10 h-10 m-1 ${color === "bg-white" ? color : color + colorLevel} hover:opacity-75 rounded-full shadow-md`}
+                onClick={() => handleColorChange(color)}
+              ></button>
+            ))}
+          </Box>
+          <Button sx={{ marginTop: 4 }} color="secondary" variant="contained" onClick={handleSubmit}>
+            <span>Preview</span>
+            <ChevronRightRoundedIcon className="-translate-y-[1px]" />
+          </Button>
+        </Box>
       </div>
     </div>
   );
-}
-
-
+};
 
 interface TabPanelProps {
   children?: React.ReactNode;
@@ -123,35 +183,45 @@ const MenuDisplay = ({ onDressUp }: { onDressUp: (part: string, outfit: string) 
   };
 
   return (
-    <Box sx={{ display: 'flex', height: '100%', width: 300 }} className='border shadow-md bg-slate-100 rounded-lg'>
-      {Object.keys(actions).map((part, index) => (
-        <TabPanel key={index} value={value} index={index}>
-          <ImageList sx={{ width: 130, height: 440 }} cols={1}>
-            {actions[part as keyof typeof actions].map((item: string) => (
-              <ImageListItem key={item} onClick={() => onDressUp(part, item)}>
-                {item !== "" ?
-                  <img srcSet={item} src={item} alt='outfit' className='hover:bg-slate-200 cursor-pointer' loading="lazy" />
-                  : <DoNotDisturbAltIcon className='w-full h-full text-red-600 hover:bg-slate-200 cursor-pointer p-3' />}
-              </ImageListItem>
-            ))}
-          </ImageList>
-        </TabPanel>
-      ))}
+    <Box sx={{ display: 'flex', height: 'auto', width: '100%', overflow: 'hidden', }} className='flex-col border shadow-md bg-slate-100 rounded-lg'>
       <Tabs
-        orientation="vertical"
+        orientation="horizontal"
         variant="scrollable"
         value={value}
         onChange={handleChange}
         textColor="inherit"
         indicatorColor="secondary"
         aria-label="Parts container"
-        sx={{ borderLeft: 1, borderColor: 'divider', width: 100 }}
+        sx={{ borderBottom: 1, borderColor: 'divider', width: '100%' }}
         className='border-[#9c27b0] text-[#9c27b0]'
       >
         {Object.keys(actions).map((part, index) => (
           <Tab key={index} label={part} {...a11yProps(index)} />
         ))}
       </Tabs>
+      {Object.keys(actions).map((part, index) => (
+        <TabPanel key={index} value={value} index={index}>
+          <div className="flex flex-row w-full justify-start overflow-x-auto hide-scrollbar"> {/* Overflow-x added for horizontal scrolling */}
+            {actions[part as keyof typeof actions].map((item: string) => (
+              <>
+                {item !== "" ?
+                <Avatar 
+                  key={item} 
+                  src={item} 
+                  onClick={() => onDressUp(part, item)} 
+                  alt='outfit' 
+                  className='hover:bg-slate-200 hover:rounded-none cursor-pointer w-[100px] h-[150px] mx-2' 
+                  sx={{ flexShrink: 0 }}  // Ensure avatars do not shrink to fit
+                />
+                : <DoNotDisturbAltIcon 
+                    className='text-red-600 hover:bg-slate-200 cursor-pointer w-[100px] h-[150px] mx-2' 
+                    sx={{ flexShrink: 0 }} 
+                  />}
+              </>
+            ))}
+          </div>
+        </TabPanel>
+      ))}
     </Box>
   );
 };
@@ -195,41 +265,15 @@ const DressUpDisplay = ({ outfits, backColor, setBackColor }: { outfits: { [key:
     // console.log(backgroundColor, colorLevel, backColor, "---Color");
   }
   return (
-    <div className={`border shadow-md rounded-lg ${backgroundColor === "bg-white" ? backgroundColor : backgroundColor + colorLevel}`}>
-      <div className="relative w-[20em] pointer-events-none">
+    <div style={{zIndex: 3}} className={`w-full flex items-start justify-center h-[420px] border shadow-md rounded-lg ${backgroundColor === "bg-white" ? backgroundColor : backgroundColor + colorLevel}`}>
+      <div className="relative w-[20em] h-[420px] pointer-events-none">
         {Object.keys(outfits).map((part, index) => (
           <div key={index}>
             {outfits[part] !== "" ?
-              <img src={outfits[part]} alt={part} className="absolute -top-12 w-full" />
+              <img src={outfits[part]} alt={part} style={{marginTop: '-64px'}} className="absolute w-full" />
               : <></>}
           </div>
         ))}
-      </div>
-      <div className="absolute w-full bottom-6 left-1/2 -translate-x-1/2">
-        <Box className="relative flex flex-col items-center w-[95%] left-1/2 -translate-x-1/2 bg-slate-300 p-4 border shadow-md rounded-lg">
-          <Slider
-            aria-label="Color Intensity"
-            defaultValue={500}
-            onChange={handleIntensityChange}
-            step={100}
-            marks
-            min={100}
-            max={900}
-            color="secondary"
-            valueLabelDisplay="off"
-          />
-          <Typography className="text-[#9c27b0] -translate-y-1">Change Intensity</Typography>
-          <Box className="grid grid-cols-11 border p-2 bg-slate-200 rounded-md">
-            {backgroundColors.map((color, index) => (
-              <button
-                key={index}
-                className={`w-10 h-10 m-1 ${color === "bg-white" ? color : color + colorLevel} hover:opacity-75 rounded-full shadow-md`}
-                onClick={() => handleColorChange(color)}
-              >
-              </button>
-            ))}
-          </Box>
-        </Box>
       </div>
     </div>
   );
--- src/blocks/assets/Wardrobe/dress.js
+++ src/blocks/assets/Wardrobe/dress.js
@@ -1,118 +1,118 @@
 // Body
-import body1 from "./basics_body1.png";
-import body2 from "./basics_body2.png";
-import body3 from "./basics_body3.png";
-import body4 from "./basics_body4.png";
-import body5 from "./basics_body5.png";
+const body1 = "https://storage.googleapis.com/miniapp-resources/Wardrobe/body/basics_body1.png";
+const body2 = "https://storage.googleapis.com/miniapp-resources/Wardrobe/body/basics_body2.png";
+const body3 = "https://storage.googleapis.com/miniapp-resources/Wardrobe/body/basics_body3.png";
+const body4 = "https://storage.googleapis.com/miniapp-resources/Wardrobe/body/basics_body4.png";
+const body5 = "https://storage.googleapis.com/miniapp-resources/Wardrobe/body/basics_body5.png";
 
 // Hairs
-import hairA_1 from "./basics_hairA_1.png";
-import hairA_2 from "./basics_hairA_2.png";
-import hairA_3 from "./basics_hairA_3.png";
-import hairA_4 from "./basics_hairA_4.png";
-import hairA_5 from "./basics_hairA_5.png";
-import hairB_1 from "./basics_hairB_1.png";
-import hairB_2 from "./basics_hairB_2.png";
-import hairB_3 from "./basics_hairB_3.png";
-import hairB_4 from "./basics_hairB_4.png";
-import hairC_1 from "./basics_hairC_1.png";
-import hairC_2 from "./basics_hairC_2.png";
-import hairC_3 from "./basics_hairC_3.png";
-import hairC_4 from "./basics_hairC_4.png";
-import hairC_5 from "./basics_hairC_5.png";
-import hairD_1 from "./basics_hairD_1.png";
-import hairD_2 from "./basics_hairD_2.png";
-import hairD_3 from "./basics_hairD_3.png";
-import hairD_4 from "./basics_hairD_4.png";
-import hairD_5 from "./basics_hairD_5.png";
-import hairE_1 from "./basics_hairE_1.png";
-import hairE_2 from "./basics_hairE_2.png";
-import hairE_3 from "./basics_hairE_3.png";
-import hairE_4 from "./basics_hairE_4.png";
-import hairE_5 from "./basics_hairE_5.png";
+const hairA_1 = "https://storage.googleapis.com/miniapp-resources/Wardrobe/hair/basics_hairA_1.png";
+const hairA_2 = "https://storage.googleapis.com/miniapp-resources/Wardrobe/hair/basics_hairA_2.png";
+const hairA_3 = "https://storage.googleapis.com/miniapp-resources/Wardrobe/hair/basics_hairA_3.png";
+const hairA_4 = "https://storage.googleapis.com/miniapp-resources/Wardrobe/hair/basics_hairA_4.png";
+const hairA_5 = "https://storage.googleapis.com/miniapp-resources/Wardrobe/hair/basics_hairA_5.png";
+const hairB_1 = "https://storage.googleapis.com/miniapp-resources/Wardrobe/hair/basics_hairB_1.png";
+const hairB_2 = "https://storage.googleapis.com/miniapp-resources/Wardrobe/hair/basics_hairB_2.png";
+const hairB_3 = "https://storage.googleapis.com/miniapp-resources/Wardrobe/hair/basics_hairB_3.png";
+const hairB_4 = "https://storage.googleapis.com/miniapp-resources/Wardrobe/hair/basics_hairB_4.png";
+const hairC_1 = "https://storage.googleapis.com/miniapp-resources/Wardrobe/hair/basics_hairC_1.png";
+const hairC_2 = "https://storage.googleapis.com/miniapp-resources/Wardrobe/hair/basics_hairC_2.png";
+const hairC_3 = "https://storage.googleapis.com/miniapp-resources/Wardrobe/hair/basics_hairC_3.png";
+const hairC_4 = "https://storage.googleapis.com/miniapp-resources/Wardrobe/hair/basics_hairC_4.png";
+const hairC_5 = "https://storage.googleapis.com/miniapp-resources/Wardrobe/hair/basics_hairC_5.png";
+const hairD_1 = "https://storage.googleapis.com/miniapp-resources/Wardrobe/hair/basics_hairD_1.png";
+const hairD_2 = "https://storage.googleapis.com/miniapp-resources/Wardrobe/hair/basics_hairD_2.png";
+const hairD_3 = "https://storage.googleapis.com/miniapp-resources/Wardrobe/hair/basics_hairD_3.png";
+const hairD_4 = "https://storage.googleapis.com/miniapp-resources/Wardrobe/hair/basics_hairD_4.png";
+const hairD_5 = "https://storage.googleapis.com/miniapp-resources/Wardrobe/hair/basics_hairD_5.png";
+const hairE_1 = "https://storage.googleapis.com/miniapp-resources/Wardrobe/hair/basics_hairE_1.png";
+const hairE_2 = "https://storage.googleapis.com/miniapp-resources/Wardrobe/hair/basics_hairE_2.png";
+const hairE_3 = "https://storage.googleapis.com/miniapp-resources/Wardrobe/hair/basics_hairE_3.png";
+const hairE_4 = "https://storage.googleapis.com/miniapp-resources/Wardrobe/hair/basics_hairE_4.png";
+const hairE_5 = "https://storage.googleapis.com/miniapp-resources/Wardrobe/hair/basics_hairE_5.png";
 
 // Eyes
-import eyes1 from "./basics_eyes1.png";
-import eyes2 from "./basics_eyes2.png";
-import eyes3 from "./basics_eyes3.png";
-import eyes4 from "./basics_eyes4.png";
-import eyes5 from "./basics_eyes5.png";
+const eyes1 = "https://storage.googleapis.com/miniapp-resources/Wardrobe/eyes/basics_eyes1.png";
+const eyes2 = "https://storage.googleapis.com/miniapp-resources/Wardrobe/eyes/basics_eyes2.png";
+const eyes3 = "https://storage.googleapis.com/miniapp-resources/Wardrobe/eyes/basics_eyes3.png";
+const eyes4 = "https://storage.googleapis.com/miniapp-resources/Wardrobe/eyes/basics_eyes4.png";
+const eyes5 = "https://storage.googleapis.com/miniapp-resources/Wardrobe/eyes/basics_eyes5.png";
 
 // Eyebrows
-import eyebrow1 from "./basics_eyebrow1.png";
-import eyebrow2 from "./basics_eyebrow2.png";
-import eyebrow3 from "./basics_eyebrow3.png";
-import eyebrow4 from "./basics_eyebrow4.png";
-import eyebrow5 from "./basics_eyebrow5.png";
+const eyebrow1 = "https://storage.googleapis.com/miniapp-resources/Wardrobe/eyebrows/basics_eyebrow1.png";
+const eyebrow2 = "https://storage.googleapis.com/miniapp-resources/Wardrobe/eyebrows/basics_eyebrow2.png";
+const eyebrow3 = "https://storage.googleapis.com/miniapp-resources/Wardrobe/eyebrows/basics_eyebrow3.png";
+const eyebrow4 = "https://storage.googleapis.com/miniapp-resources/Wardrobe/eyebrows/basics_eyebrow4.png";
+const eyebrow5 = "https://storage.googleapis.com/miniapp-resources/Wardrobe/eyebrows/basics_eyebrow5.png";
 
 // Eyelashes
-import eyelashes1 from "./basics_eyelashes1.png";
-import eyelashes2 from "./basics_eyelashes2.png";
-import eyelashes3 from "./basics_eyelashes3.png";
-import eyelashes4 from "./basics_eyelashes4.png";
-import eyelashes5 from "./basics_eyelashes5.png";
+const eyelashes1 = "https://storage.googleapis.com/miniapp-resources/Wardrobe/eyelashes/basics_eyelashes1.png";
+const eyelashes2 = "https://storage.googleapis.com/miniapp-resources/Wardrobe/eyelashes/basics_eyelashes2.png";
+const eyelashes3 = "https://storage.googleapis.com/miniapp-resources/Wardrobe/eyelashes/basics_eyelashes3.png";
+const eyelashes4 = "https://storage.googleapis.com/miniapp-resources/Wardrobe/eyelashes/basics_eyelashes4.png";
+const eyelashes5 = "https://storage.googleapis.com/miniapp-resources/Wardrobe/eyelashes/basics_eyelashes5.png";
 
 // Mouths
-import mouth1 from "./basics_mouth1.png";
-import mouth2 from "./basics_mouth2.png";
-import mouth3 from "./basics_mouth3.png";
-import mouth4 from "./basics_mouth4.png";
+const mouth1 = "https://storage.googleapis.com/miniapp-resources/Wardrobe/mouths/basics_mouth1.png";
+const mouth2 = "https://storage.googleapis.com/miniapp-resources/Wardrobe/mouths/basics_mouth2.png";
+const mouth3 = "https://storage.googleapis.com/miniapp-resources/Wardrobe/mouths/basics_mouth3.png";
+const mouth4 = "https://storage.googleapis.com/miniapp-resources/Wardrobe/mouths/basics_mouth4.png";
 
 // Top
-import top1 from "./basics_top1.png";
-import top2 from "./basics_top2.png";
-import top3 from "./basics_top3.png";
-import top4 from "./basics_top4.png";
-import top5 from "./basics_top5.png";
-import topB_1 from "./basics_topB_1.png";
-import topB_2 from "./basics_topB_2.png";
-import topB_3 from "./basics_topB_3.png";
-import topB_4 from "./basics_topB_4.png";
-import topB_5 from "./basics_topB_5.png";
-import topBBW from "./basics_topBBW.png";
-import topBW from "./basics_topBW.png";
-import topC_1 from "./basics_topC_1.png";
-import topC_2 from "./basics_topC_2.png";
-import topC_3 from "./basics_topC_3.png";
-import topC_4 from "./basics_topC_4.png";
-import topC_5 from "./basics_topC_5.png";
+const top1 = "https://storage.googleapis.com/miniapp-resources/Wardrobe/top/basics_top1.png";
+const top2 = "https://storage.googleapis.com/miniapp-resources/Wardrobe/top/basics_top2.png";
+const top3 = "https://storage.googleapis.com/miniapp-resources/Wardrobe/top/basics_top3.png";
+const top4 = "https://storage.googleapis.com/miniapp-resources/Wardrobe/top/basics_top4.png";
+const top5 = "https://storage.googleapis.com/miniapp-resources/Wardrobe/top/basics_top5.png";
+const topB_1 = "https://storage.googleapis.com/miniapp-resources/Wardrobe/top/basics_topB_1.png";
+const topB_2 = "https://storage.googleapis.com/miniapp-resources/Wardrobe/top/basics_topB_2.png";
+const topB_3 = "https://storage.googleapis.com/miniapp-resources/Wardrobe/top/basics_topB_3.png";
+const topB_4 = "https://storage.googleapis.com/miniapp-resources/Wardrobe/top/basics_topB_4.png";
+const topB_5 = "https://storage.googleapis.com/miniapp-resources/Wardrobe/top/basics_topB_5.png";
+const topC_1 = "https://storage.googleapis.com/miniapp-resources/Wardrobe/top/basics_topC_1.png";
+const topC_2 = "https://storage.googleapis.com/miniapp-resources/Wardrobe/top/basics_topC_2.png";
+const topC_3 = "https://storage.googleapis.com/miniapp-resources/Wardrobe/top/basics_topC_3.png";
+const topC_4 = "https://storage.googleapis.com/miniapp-resources/Wardrobe/top/basics_topC_4.png";
+const topC_5 = "https://storage.googleapis.com/miniapp-resources/Wardrobe/top/basics_topC_5.png";
 
 // Bottom
-import bottom1 from "./basics_bottom1.png";
-import bottom2 from "./basics_bottom2.png";
-import bottom3 from "./basics_bottom3.png";
-import bottom4 from "./basics_bottom4.png";
-import bottom5 from "./basics_bottom5.png";
-import bottomB_1 from "./basics_bottomB_1.png";
-import bottomB_2 from "./basics_bottomB_2.png";
-import bottomB_3 from "./basics_bottomB_3.png";
-import bottomB_4 from "./basics_bottomB_4.png";
-import bottomB_5 from "./basics_bottomB_5.png";
-import bottomC_1 from "./basics_bottomC_1.png";
-import bottomC_2 from "./basics_bottomC_2.png";
-import bottomC_3 from "./basics_bottomC_3.png";
-import bottomC_4 from "./basics_bottomC_4.png";
-import bottomC_5 from "./basics_bottomC_5.png";
+const bottom1 = "https://storage.googleapis.com/miniapp-resources/Wardrobe/bottom/basics_bottom1.png";
+const bottom2 = "https://storage.googleapis.com/miniapp-resources/Wardrobe/bottom/basics_bottom2.png";
+const bottom3 = "https://storage.googleapis.com/miniapp-resources/Wardrobe/bottom/basics_bottom3.png";
+const bottom4 = "https://storage.googleapis.com/miniapp-resources/Wardrobe/bottom/basics_bottom4.png";
+const bottom5 = "https://storage.googleapis.com/miniapp-resources/Wardrobe/bottom/basics_bottom5.png";
+const bottomB_1 = "https://storage.googleapis.com/miniapp-resources/Wardrobe/bottom/basics_bottomB_1.png";
+const bottomB_2 = "https://storage.googleapis.com/miniapp-resources/Wardrobe/bottom/basics_bottomB_2.png";
+const bottomB_3 = "https://storage.googleapis.com/miniapp-resources/Wardrobe/bottom/basics_bottomB_3.png";
+const bottomB_4 = "https://storage.googleapis.com/miniapp-resources/Wardrobe/bottom/basics_bottomB_4.png";
+const bottomB_5 = "https://storage.googleapis.com/miniapp-resources/Wardrobe/bottom/basics_bottomB_5.png";
+const bottomC_1 = "https://storage.googleapis.com/miniapp-resources/Wardrobe/bottom/basics_bottomC_1.png";
+const bottomC_2 = "https://storage.googleapis.com/miniapp-resources/Wardrobe/bottom/basics_bottomC_2.png";
+const bottomC_3 = "https://storage.googleapis.com/miniapp-resources/Wardrobe/bottom/basics_bottomC_3.png";
+const bottomC_4 = "https://storage.googleapis.com/miniapp-resources/Wardrobe/bottom/basics_bottomC_4.png";
+const bottomC_5 = "https://storage.googleapis.com/miniapp-resources/Wardrobe/bottom/basics_bottomC_5.png";
 
 // Shoes
-import shoes1 from "./basics_shoes1.png";
-import shoes2 from "./basics_shoes2.png";
-import shoes3 from "./basics_shoes3.png";
-import shoes4 from "./basics_shoes4.png";
-import shoes5 from "./basics_shoes5.png";
+const shoes1 = "https://storage.googleapis.com/miniapp-resources/Wardrobe/shoes/basics_shoes1.png";
+const shoes2 = "https://storage.googleapis.com/miniapp-resources/Wardrobe/shoes/basics_shoes2.png";
+const shoes3 = "https://storage.googleapis.com/miniapp-resources/Wardrobe/shoes/basics_shoes3.png";
+const shoes4 = "https://storage.googleapis.com/miniapp-resources/Wardrobe/shoes/basics_shoes4.png";
+const shoes5 = "https://storage.googleapis.com/miniapp-resources/Wardrobe/shoes/basics_shoes5.png";
 
 // Black n White
-import bodyBW from "./basics_bodyBW.png";
-import topCBW from "./basics_topCBW.png";
-import bottomBW from "./basics_bottomBW.png";
-import bottomBBW from "./basics_bottomBBW.png";
-import bottomCBW from "./basics_bottomCBW.png";
-import hairABW from "./basics_hairABW.png";
-import hairBBW from "./basics_hairBBW.png";
-import hairCBW from "./basics_hairCBW.png";
-import hairDBW from "./basics_hairDBW.png";
-import hairEBW from "./basics_hairEBW.png";
-import shoesBW from "./basics_shoesBW.png";
+const bodyBW = "https://storage.googleapis.com/miniapp-resources/Wardrobe/bnw/basics_bodyBW.png";
+const topCBW = "https://storage.googleapis.com/miniapp-resources/Wardrobe/bnw/basics_topCBW.png";
+const topBBW = "https://storage.googleapis.com/miniapp-resources/Wardrobe/bnw/basics_topBBW.png";
+const topBW = "https://storage.googleapis.com/miniapp-resources/Wardrobe/bnw/basics_topBW.png";
+const bottomBW = "https://storage.googleapis.com/miniapp-resources/Wardrobe/bnw/basics_bottomBW.png";
+const bottomBBW = "https://storage.googleapis.com/miniapp-resources/Wardrobe/bnw/basics_bottomBBW.png";
+const bottomCBW = "https://storage.googleapis.com/miniapp-resources/Wardrobe/bnw/basics_bottomCBW.png";
+const hairABW = "https://storage.googleapis.com/miniapp-resources/Wardrobe/bnw/basics_hairABW.png";
+const hairBBW = "https://storage.googleapis.com/miniapp-resources/Wardrobe/bnw/basics_hairBBW.png";
+const hairCBW = "https://storage.googleapis.com/miniapp-resources/Wardrobe/bnw/basics_hairCBW.png";
+const hairDBW = "https://storage.googleapis.com/miniapp-resources/Wardrobe/bnw/basics_hairDBW.png";
+const hairEBW = "https://storage.googleapis.com/miniapp-resources/Wardrobe/bnw/basics_hairEBW.png";
+const shoesBW = "https://storage.googleapis.com/miniapp-resources/Wardrobe/bnw/basics_shoesBW.png";
 
 const Char_Body = [body1, body2, body3, body4, body5, bodyBW];
 const Char_Hair = ["", hairA_1, hairA_2, hairA_3, hairA_4, hairA_5, hairABW, hairB_1, hairB_2, hairB_3, hairB_4, hairBBW, hairC_1, hairC_2, hairC_3, hairC_4, hairC_5, hairCBW, hairD_1, hairD_2, hairD_3, hairD_4, hairD_5, hairDBW, hairE_1, hairE_2, hairE_3, hairE_4, hairE_5, hairEBW];
@@ -123,6 +123,6 @@ const Char_Mouth = ["", mouth1, mouth2, mouth3, mouth4];
 const Char_Top = [top1, top2, top3, top4, top5, topBW, topB_1, topB_2, topB_3, topB_4, topB_5, topBBW, topC_1, topC_2, topC_3, topC_4, topC_5, topCBW];
 const Char_Bottom = [bottom1, bottom2, bottom3, bottom4, bottom5, bottomBW, bottomB_1, bottomB_2, bottomB_3, bottomB_4, bottomB_5, bottomBBW, bottomC_1, bottomC_2, bottomC_3, bottomC_4, bottomC_5, bottomCBW];
 const Char_Shoes = ["", shoes1, shoes2, shoes3, shoes4, shoes5, shoesBW];
-const Char_BW = [ bodyBW, eyes1, eyebrow1, eyelashes1, hairABW, mouth1, topBW, bottomBW, shoesBW];
+const Char_BW = [bodyBW, eyes1, eyebrow1, eyelashes1, hairABW, mouth1, topBW, bottomBW, shoesBW];
 
-export { Char_Body, Char_Hair, Char_Eyes, Char_Eyebrows, Char_Eyelashes, Char_Mouth, Char_Top, Char_Bottom, Char_Shoes, Char_BW};
+export { Char_Body, Char_Hair, Char_Eyes, Char_Eyebrows, Char_Eyelashes, Char_Mouth, Char_Top, Char_Bottom, Char_Shoes, Char_BW };
