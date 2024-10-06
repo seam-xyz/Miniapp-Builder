d71f34003a5676037cc4f3c329e841ca86697cf9
Daniel Gavidia <dgavidia1@gmail.com>
2024-10-06 19:35:54 -0400

    Passed model data into feed component

---
+++ src/blocks/utils/SeamMeme/components/AppFeed.tsx
@@ -0,0 +1,21 @@
+import { BlockModel } from "../../../types";
+
+interface AppFeedProps {
+	model: BlockModel;
+	update: (data: { [key: string]: string }) => void;
+}
+
+const AppFeed = ({ model, update }: AppFeedProps) => {
+	const caption = model.data.caption;
+	const editedMeme = model.data.editedMeme;
+	return (
+		<div>
+			<div>{caption}</div>
+			<div>
+				<img src={editedMeme} />
+			</div>
+		</div>
+	);
+};
+
+export default AppFeed;
