8bca0f9b06cc510fef3a4625ab7e07906ab84017
James Buret <james@getseam.xyz>
2024-10-10 16:07:12 -0400

    allow for seampillbutton to be recolored via props

--- src/components/SeamPillButton.jsx
+++ src/components/SeamPillButton.jsx
@@ -1,16 +1,17 @@
-const SeamPillButton = ({ icon, label, onClick, colorOverride }) => {
+const SeamPillButton = ({ icon, label, onClick, textColor, colorOverride, isOnChannelPage, }) => {
   return (
-    <button
+    <div
       onClick={onClick}
-      className="flex items-center px-3 py-2 gap-2 rounded-full cursor-pointer border-0"
+      className={`${isOnChannelPage && `bg-${textColor} bg-opacity-20`} flex border border-black/10 items-center w-auto h-[40px] cursor-pointer rounded-full`}
       style={{
-        borderRadius: '43px',
-        backgroundColor: colorOverride ? colorOverride : '#EFEFEF', // Use `colorOverride` if provided, else fallback to default color
+        backgroundColor: colorOverride && colorOverride, // Use `colorOverride` if provided, else fallback to default color
       }}
     >
+      <div className={`flex flex-row items-center justify-center space-x-2 m-3`}>
       {icon}
-      {label && <span className="text-black text-sm leading-normal font-normal">{label}</span>}
-    </button>
+      {label && <h4 className={`text-${textColor} text-center leading-normal font-normal`}>{label}</h4>}
+      </div>
+    </div>
   );
 };
 
