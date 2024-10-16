eaa263b7702ce715a057755a09a480380708e3e6
Daniel Gavidia <dgavidia1@gmail.com>
2024-10-16 16:13:52 -0400

    Fixed VSCode settings and renamed imports/files for SeamMeme, now called MemeGen

--- src/blocks/BlockFactory.tsx
+++ src/blocks/BlockFactory.tsx
@@ -1,4 +1,4 @@
-import { SeamFeedComponent, SeamComposerComponent } from './SeamMeme'
+import { MemeGenFeedComponent, MemeGenComposerComponent } from './MemeGenApp'
 import { LayeredImageFeedComponent, LayeredImageComposerComponent } from './LayeredImageBlock'
 import { ColorFeedComponent, ColorComposerComponent } from './ColorApp'
 import { RiddlequestFeedComponent, RiddlequestComposerComponent } from './RiddlequestApp'
@@ -101,7 +101,7 @@ export default class BlockFactory {
       case "Pickrr": return <PickrrFeedComponent model={model} update={update} />;
       case "Riddlequest": return <RiddlequestFeedComponent model={model} update={update} />;
       case "Color": return <ColorFeedComponent model={model} update={update} />;
-      case "Seam": return <SeamFeedComponent model={model} update={update} />;
+      case "MemeGen": return <MemeGenFeedComponent model={model} update={update} />;
       case "ReviewThings": return <ReviewThingsAppFeedComponent model={model} update={update} />;
       // new feed components go here
       default: return UnknownFeedComponent({ model, update });
@@ -158,7 +158,7 @@ export default class BlockFactory {
       case "MagicCard": return MagicCardComposerComponent(props);
       case "Horoscope": return HoroscopeComposerComponent(props);
       case "Dizzy": return DizzyComposerComponent(props);
-      case "Seam": return SeamComposerComponent(props);
+      case "MemeGen": return MemeGenComposerComponent(props);
       case "ReviewThings": return ReviewThingsAppComposerComponent(props);
       // new composer components go here
       default: return UnknownComposerComponent(props);
--- src/blocks/MemeGenApp.tsx
+++ src/blocks/MemeGenApp.tsx
@@ -2,7 +2,7 @@ import { BlockModel, ComposerComponentProps, FeedComponentProps } from "./types"
 import AppComposer from "./utils/SeamMeme/components/AppComposer";
 import AppFeed from "./utils/SeamMeme/components/AppFeed";
 
-export const SeamComposerComponent = ({ model, done }: ComposerComponentProps) => {
+export const MemeGenComposerComponent = ({ model, done }: ComposerComponentProps) => {
 	return (
 		<div className="h-full w-full">
 			<AppComposer model={model} done={done} />
@@ -10,7 +10,7 @@ export const SeamComposerComponent = ({ model, done }: ComposerComponentProps) =
 	);
 };
 
-export const SeamFeedComponent = ({ model, update }: FeedComponentProps) => {
+export const MemeGenFeedComponent = ({ model, update }: FeedComponentProps) => {
 	return (
 		<div>
 			<AppFeed model={model} update={update} />
