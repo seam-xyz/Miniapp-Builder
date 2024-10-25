8f7a8c240df59486e310b398ebde0275bcf48a3c
James Buret <james@getseam.xyz>
2024-10-25 14:35:16 -0400

    added darkmode prop to miniapp builder

--- src/components/SeamPillButton.jsx
+++ src/components/SeamPillButton.jsx
@@ -1,15 +1,17 @@
-const SeamPillButton = ({ icon, label, onClick, textColor, colorOverride, isOnChannelPage, }) => {
+const SeamPillButton = ({ icon, label, onClick, darkMode, colorOverride, isOnChannelPage, }) => {
+  let color = darkMode ? 'white' : 'black';
+
   return (
     <div
       onClick={onClick}
-      className={`${isOnChannelPage && `bg-${textColor} bg-opacity-20`} flex border border-black/10 items-center w-auto h-[40px] cursor-pointer rounded-full`}
+      className={`${isOnChannelPage && `bg-${color} bg-opacity-20`} flex border border-black/10 items-center w-auto h-[40px] cursor-pointer rounded-full`}
       style={{
         backgroundColor: colorOverride && colorOverride, // Use `colorOverride` if provided, else fallback to default color
       }}
     >
       <div className={`flex flex-row items-center justify-center space-x-2 m-3`}>
       {icon}
-      {label && <h4 className={`text-${textColor} text-center leading-normal font-normal`}>{label}</h4>}
+      {label && <h4 className={`text-${color} text-center leading-normal font-normal`}>{label}</h4>}
       </div>
     </div>
   );
